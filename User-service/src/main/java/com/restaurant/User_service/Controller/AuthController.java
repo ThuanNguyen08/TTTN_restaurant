aaaca89d4fdd9ece252541d423e4fbf48abb9bee
ThuanNguyen08 <cntt44dnguyenvanthuan0990@gmail.com>
2025-05-11 22:06:13 +0700

    add revoke token

--- User-service/src/main/java/com/restaurant/User_service/Controller/AuthController.java
+++ User-service/src/main/java/com/restaurant/User_service/Controller/AuthController.java
@@ -7,10 +7,12 @@ import com.restaurant.User_service.Entity.RefreshToken;
 import com.restaurant.User_service.Exeption.TokenRefreshException;
 import com.restaurant.User_service.Security.JwtUtil;
 import com.restaurant.User_service.Service.RefreshTokenService;
-import com.restaurant.User_service.Service.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,35 +26,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 @Tag(name = "Token", description = "Refresh Token")
 public class AuthController {
-    private final RefreshTokenService refreshTokenService;
-    private final JwtUtil jwtUtil;
-    private final UserService userService;
-    private final UserDetailsService userDetailsService;
+	private final RefreshTokenService refreshTokenService;
+	private final JwtUtil jwtUtil;
+	private final UserDetailsService userDetailsService;
 
-    public AuthController(RefreshTokenService refreshTokenService, JwtUtil jwtUtil, 
-                         UserService userService, UserDetailsService userDetailsService) {
-        this.refreshTokenService = refreshTokenService;
-        this.jwtUtil = jwtUtil;
-        this.userService = userService;
-        this.userDetailsService = userDetailsService;
-    }
+	public AuthController(RefreshTokenService refreshTokenService, JwtUtil jwtUtil,
+			UserDetailsService userDetailsService) {
+		this.refreshTokenService = refreshTokenService;
+		this.jwtUtil = jwtUtil;
+		this.userDetailsService = userDetailsService;
+	}
 
-    @Operation(summary = "Tạo access token mới từ bằng refresh token cũ", 
-    		description = "Có thể tạo một access token mới từ refresh token cũ còn hạn của người dùng có từ lần đăng nhập trước")
-    @PostMapping("/refresh-token")
-    public ResponseEntity<?> refreshToken(@RequestBody TokenRefreshRequest request) {
-        String requestRefreshToken = request.getRefreshToken();
+	@Operation(summary = "Tạo access token mới từ bằng refresh token cũ", description = "Có thể tạo một access token mới từ refresh token cũ còn hạn của người dùng có từ lần đăng nhập trước")
+	@PostMapping("/refresh-token")
+	public ResponseEntity<?> refreshToken(@RequestBody TokenRefreshRequest request) {
+		String requestRefreshToken = request.getRefreshToken();
+		if (requestRefreshToken == null) {
+			return ResponseEntity.badRequest().body(Map.of("message", "Refresh token không được để trống"));
+		}
 
-        return refreshTokenService.findByToken(requestRefreshToken)
-                .map(refreshTokenService::verifyExpiration)
-                .map(RefreshToken::getUser)
-                .map(user -> {
-                    UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
-                    String newAccessToken = jwtUtil.generateToken(userDetails, user);
-                    
-                    return ResponseEntity.ok(new TokenRefreshResponse(newAccessToken, requestRefreshToken));
-                })
-                .orElseThrow(() -> new TokenRefreshException(requestRefreshToken,
-                        "Refresh token không tồn tại trong cơ sở dữ liệu!"));
-    }
+		return refreshTokenService.findByToken(requestRefreshToken).map(refreshTokenService::verifyExpiration)
+				.map(RefreshToken::getUser).map(user -> {
+					 if (!user.getIsActive()) {
+			                throw new TokenRefreshException(requestRefreshToken, "Tài khoản đã bị vô hiệu hóa");
+			            }
+					UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+					String newAccessToken = jwtUtil.generateToken(userDetails, user);
+
+					return ResponseEntity.ok(new TokenRefreshResponse(newAccessToken, requestRefreshToken));
+				}).orElseThrow(() -> new TokenRefreshException(requestRefreshToken,
+						"Refresh token không tồn tại trong cơ sở dữ liệu!"));
+	}
+	
+	@PostMapping("/revoke-token")
+	public ResponseEntity<?> revokeToken(HttpServletRequest request) {
+	    Long userId = (Long) request.getAttribute("userId");
+	    refreshTokenService.deleteByUserId(userId);
+	    return ResponseEntity.ok(Map.of("message", "Refresh token đã được thu hồi"));
+	}
 }
